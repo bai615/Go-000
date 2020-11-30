42ba1f0430e31e37de9dc5725b91b5d01c955dfc
白华 <baihua@k12studio.com>
2020-11-30 20:57:55 +0800

    Week02

---
+++ Week02/main01.go
@@ -0,0 +1,39 @@
+﻿package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	err := Biz()
+	fmt.Printf("service: %+v\n", err)
+}
+
+type user struct {
+	id   int
+	name string
+	age  int
+}
+
+var ErrNotFound = errors.New("Record not found!")
+
+func Dao() (error) {
+	return errors.Wrap(ErrNotFound, "SQL failed query!")
+}
+
+var ErrNotWork = errors.New("Too young to work!")
+
+func Biz() error {
+	u := &user{id: 1, name: "Tom", age: 17}
+
+	err := Dao()
+	if err != nil {
+		return err
+	}
+
+	if u.age < 18 {
+		return errors.Wrap(ErrNotWork, "")
+	}
+	return nil
+}
